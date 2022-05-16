d0d76a10f73abc1bcc3396544e2f03dfb4c23279
«Vova <vrobskiy@gmail.com>
2022-05-16 22:06:59 +0300

    add task final arrays [,]

---
+++ 002/Program.cs
@@ -0,0 +1,76 @@
+﻿
+// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+// 0 встречается 2 раза 
+// 1 встречается 1 раз 
+// 2 встречается 1 раз 
+// 8 встречается 1 раз 
+// 9 встречается 3 раза
+
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+Console.Write("Введите количество строк m: "); 
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m,n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+double length = 10;
+
+for (double i = 0; i < length; i++)
+{
+    double result = CheckNumber(i);
+    if (result != 0)
+        Console.WriteLine($"Введенное число {i} встречается {result} раз. ");
+}
+double CheckNumber(double number)
+{   
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (number == array[i, j])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+void PrintArray(double[,] matr) 
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($" {matr[rows, columns]} ");   
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(double[,] matr) 
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows,columns] = new Random().Next(0,10); 
+        }
+        Console.WriteLine();
+    }
+}
+
+
